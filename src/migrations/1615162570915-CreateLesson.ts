8c90026043e48fdb2ac08b3224e4500e1b67525c
lexdmm <lexdmm@users.noreply.github.com>
2021-03-07 21:34:25 -0300

    criar tabela lesson: npm run typeorm migration:run

---
+++ src/migrations/1615162570915-CreateLesson.ts
@@ -0,0 +1,29 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm';
+
+export class CreateLesson1615162570915 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({ 
+                name: 'lesson',
+                columns: [
+                    {
+                        name: 'idAula',
+                        type: 'bigint',
+                        isPrimary: true,
+                        generationStrategy: 'increment'
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar(255)'
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('lesson')
+    }
+
+}
