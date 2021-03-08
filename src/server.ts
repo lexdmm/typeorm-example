3c4233f108dbac01454beee76e644782c808b74c
lexdmm <lexdmm@users.noreply.github.com>
2021-03-07 22:26:47 -0300

    habilitar database para evitar problema de conexão

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from './app';
 import 'reflect-metadata';
-// import './database';
+import './database'; // para evitar erro ao tentar conectar
 
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('🏃 Running Server');
 });
